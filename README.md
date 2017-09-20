84b551d93c26628326231adf9427624fb4f8fd31
AndrewPopovich <andrew1994popovich@gmail.com>
2017-09-20 15:14:03 +0300

    Fixed bug with test config

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # hello_back_end
+[![Build Status](https://travis-ci.org/AndrewPopovich/hello_back_end.svg?branch=master)](https://travis-ci.org/AndrewPopovich/hello_back_end)
--- src/test/java/ua/andrew/hellobackend/daoimpl/ContactsDAOImplTest.java
+++ src/test/java/ua/andrew/hellobackend/daoimpl/ContactsDAOImplTest.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.SpringApplicationConfiguration;
 import org.springframework.context.ApplicationContext;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
 import ua.andrew.hellobackend.Application;
 import ua.andrew.hellobackend.dao.ContactsDAO;
 
@@ -17,6 +18,7 @@ import java.sql.Statement;
 
 import static org.junit.Assert.assertEquals;
 
+@WebAppConfiguration
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringApplicationConfiguration(classes = Application.class)
 public class ContactsDAOImplTest {
